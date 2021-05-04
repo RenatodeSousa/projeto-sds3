297444d1b2a0dfade58637a5b830ce0fa97ad004
Renato Sousa <renatodesousadasilva.1@gmail.com>
2021-05-03 22:28:14 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import DataTable from "./components/DataTable";
+
 function App() {
-  return (
-    <div >
-      <h1 className="text-primary">Teste Fronte</h1>
-    </div>
-  );
+    return (
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary">Teste Fronte</h1>
+                <DataTable/>
+            </div>
+            <Footer/>
+
+        </>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import ImgDsDark from 'assets/img/ds_dark.svg'
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por<a href="https://github.com/RenatodeSousa/" target="_blank" rel="noreferrer"> Renato de Sousa</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
